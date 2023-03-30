f9c2fe4a07e6f2ed747e3ee99c1907548b4bf907
Olexandr <1andraste1@gmail.com>
2023-03-30 16:35:50 +0300

    Created maze generator

---
+++ Assets/LevelCreator/MazeGenerator.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGeneratorCell
+{
+    public int x;
+    public int y;
+
+    public bool WallLeft = true;
+    public bool WallBottom = true;
+
+    public bool Visited = false;
+}
+
+public class MazeGenerator
+{
+    public int _width = 13;
+    public int _height = 12;
+
+    public MazeGeneratorCell[,] GenerateMaze()
+    {
+        MazeGeneratorCell[,] maze = new MazeGeneratorCell[_width, _height];
+
+        for (int i = 0; i < maze.GetLength(0); i++)
+        {
+            for (int j = 0; j < maze.GetLength(1); j++)
+            {
+                maze[i, j] = new MazeGeneratorCell { x = i, y = j };
+            }
+        }
+
+        for (int x = 0; x < maze.GetLength(0); x++)
+        {
+            maze[x, _height - 1].WallLeft = false;
+        }
+
+        for (int y = 0; y < maze.GetLength(1); y++)
+        {
+            maze[_width - 1, y].WallBottom = false;
+        }
+        RemoveWallsWithBacktracker(maze);
+        
+
+        return maze;
+    }
+    private void RemoveWallsWithBacktracker(MazeGeneratorCell[,] maze)
+    {
+        MazeGeneratorCell current = maze[0, 0];
+        current.Visited = true;
+
+        Stack<MazeGeneratorCell> stack = new Stack<MazeGeneratorCell>();
+        do
+        {
+            List<MazeGeneratorCell> unvisitedNeighbours = new List<MazeGeneratorCell>();
+
+            int x = current.x;
+            int y = current.y;
+
+            if (x > 0 && !maze[x - 1, y].Visited) unvisitedNeighbours.Add(maze[x - 1, y]);
+            if (y > 0 && !maze[x, y - 1].Visited) unvisitedNeighbours.Add(maze[x, y - 1]);
+            if (x < _width - 2 && !maze[x + 1, y].Visited) unvisitedNeighbours.Add(maze[x + 1, y]);
+            if (y < _height - 2 && !maze[x, y + 1].Visited) unvisitedNeighbours.Add(maze[x, y + 1]);
+
+            if (unvisitedNeighbours.Count > 0)
+            {
+                MazeGeneratorCell chosen = unvisitedNeighbours[UnityEngine.Random.Range(0, unvisitedNeighbours.Count)];
+                RemoveWall(current, chosen);
+
+                chosen.Visited = true;
+                stack.Push(chosen);
+                current = chosen;
+            }
+            else
+            {
+                current = stack.Pop();
+            }
+        } while (stack.Count > 0);
+    }
+
+    private void RemoveWall(MazeGeneratorCell a, MazeGeneratorCell b)
+    {
+        if (a.x == b.x)
+        {
+            if (a.y > b.y) a.WallBottom = false;
+            else b.WallBottom = false;
+        }
+        else
+        {
+            if (a.x > b.x) a.WallLeft = false;
+            else b.WallLeft = false;
+        }
+    }
+}
